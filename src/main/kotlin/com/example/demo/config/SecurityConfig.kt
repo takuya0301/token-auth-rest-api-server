d7bcd64c9f015642c7cba5ebdf54e0db8aecceb7
Takuya Tsuchida <takuya0301@gmail.com>
2018-07-14 18:41:22 +0900

    Add CORS support

--- src/main/kotlin/com/example/demo/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/config/SecurityConfig.kt
@@ -2,11 +2,36 @@ package com.example.demo.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.provisioning.JdbcUserDetailsManager
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
 import javax.sql.DataSource
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+        http.cors()
+        http.httpBasic()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = Arrays.asList("http://example.com")
+        configuration.allowedMethods = Arrays.asList("*")
+        configuration.allowedHeaders = Arrays.asList("*")
+        configuration.exposedHeaders = Arrays.asList("X-Auth-Token")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
     @Bean
     fun jdbcUserDetailsManager(dataSource: DataSource): JdbcUserDetailsManager {
         val jdbcUserDetailsManager = JdbcUserDetailsManager()
